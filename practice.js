1851f2eecd35f76116351dd92711f28d14407303
Yadneya Joshi <61934942+Yadnu@users.noreply.github.com>
2024-03-02 22:12:35 +0530

    Practicing TS

---
+++ practice.js
@@ -0,0 +1,11 @@
+"use strict";
+const a = 18;
+//console.log(a);
+function greet(firstName) {
+    console.log("Hello " + firstName);
+}
+greet("Yadnu");
+const v = () => {
+    return "Hello World";
+};
+console.log(v);
---
+++ practice.ts
@@ -0,0 +1,12 @@
+const a:number = 18;
+//console.log(a);
+function greet(firstName:String) {
+    console.log("Hello " + firstName);    
+}
+greet("Yadnu")
+
+
+const v:Function= () =>{
+    console.log("Hello World");
+} 
+v;
